10665e1aa802987da3d887978a4162334b9f89f1
iliasseAbdessamad <samwillh7@gmail.com>
2025-07-28 03:00:51 +0100

    Setup MainActivity.java class (Add Todo tasks)

--- app/src/main/java/net/iliasse/weatherapp/MainActivity.java
+++ app/src/main/java/net/iliasse/weatherapp/MainActivity.java
@@ -10,15 +10,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mainBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        mainBinding.buttonWeatherByCity.setOnClickListener(v -> {
+            //launch the second activity
+        });
+
+        mainBinding.buttonWeatherByLocation.setOnClickListener(v -> {
+            //ask for permission, open the second activity
         });
     }
 }
